8149f782ea9a8a370556bc5406ec8c6ca658dbe5
dpjdsa <71557388+dpjdsa@users.noreply.github.com>
2021-01-22 10:42:28 +0000

    Update Config.java

--- core/Config.java
+++ core/Config.java
@@ -8,18 +8,13 @@ import java.util.Map;
 import java.util.*;
 import java.util.concurrent.*;
 
-//import src.objective.Passenger;
-//import src.objective.PassengerList;
-//import objective.Passenger;
-//import objective.PassengerList;
-
 /**
  * MapReduce Job Configuration
  * Stores the file specifications provided at run-time and
  * uses reflection to set objective-specific mapper and reducer classes.
- *
- * Areas for improvement:
- * - Output to file or implement a user interface to display results
+ * @author BD837672
+ * @version 22nd January 2021
+ * 
  */
 public class Config {
     // Input files to process
@@ -29,49 +24,12 @@ public class Config {
     private Class mapper, reducer, combiner;
 
     // Constructor
-    //public Config(String[] args, Class mapper, Class reducer, Class combiner) {
     public Config(Class mapper, Class reducer, Class combiner) {
-        //init(args);
         this.mapper = mapper;
         this.reducer = reducer;
         this.combiner = combiner;
     }
 
-    // Initialise a job using the provided arguments
-    /*private void init(String[] args) {
-        if(args == null || args.length == 0) {
-            System.out.println("Usage: java MapReduce <files>\n\tProcess a set of files listed by <files> using a trivial MapReduce implementation.");
-            System.exit(1);
-        }
-        this.files = new File[args.length];
-        for(int i=0; i<args.length; i++)
-            this.files[i] = new File(args[i]);
-    }
-    */
-    /* Generic file reader returning an iterator cycling through each line of the specified file
-    protected static Iterator read(File file) throws IOException {
-        List record = new ArrayList();
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        String line;
-        while((line = br.readLine()) != null)
-            record.add(line);
-        br.close();
-        System.out.println("Size of file ="+record.size());
-        return record.iterator();
-    }
-    */
-    /* Generic file reader returning a list containing each line of input file
-    protected static int read(File file) throws IOException {
-    //List record = new ArrayList();
-    BufferedReader br = new BufferedReader(new FileReader(file));
-    String line;
-    while((line = br.readLine()) != null)
-        Job.record.add(line);
-    br.close();
-    System.out.println("Size of file ="+Job.record.size());
-    return Job.record.size();
-    }
-    */
     // Reads the Passenger List as if it was a file
     protected static int read(PassengerList pListIn){
         String line;
@@ -93,7 +51,6 @@ public class Config {
     //protected Mapper getMapperInstance(File file) throws Exception {
     protected Mapper getMapperInstance() throws Exception {
         Mapper mapper = (Mapper) this.mapper.getConstructor().newInstance();
-        //mapper.setFile(file);
         return mapper;
     }
 
